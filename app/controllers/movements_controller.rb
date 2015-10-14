e92163e28b5781063068ef1b1adc69ac64941264
GaboISC <11460258@itcolima.edu/mx>
2015-10-14 10:39:36 -0500

    Corrección consulta guradado

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,3 +1,21 @@
 class MovementsController < ApplicationController
+	def index
+		@movement=Movement.new
+	end
+
+	def create
+		@movement=Movement.new(movement_params)
+		 if @movement.save
+		 	redirect_to root_path
+		 else
+		 	render :new
+		 end
+	end
+
+
+private
+		def movement_params
+		params.require(:movement).permit(:movement_catalogs_id, :quantity)
+	end
 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,28 +1,12 @@
 class WelcomeController < ApplicationController
-  def index
-  	@movementcatalogs=MovementCatalog.all
-    #@movements=Movement.all
-    @movement=Movement.new
-  	@movementls=MovementCatalog.select('movement_catalogs.id','movement_catalogs.name','movements.quantity').joins('INNER JOIN movements ON movement_catalogs.id=movements.id')
-  	#@movementls=
-		
-  end
+  	def index
+  		@movementcatalogs=MovementCatalog.all
+    	#@movements=Movement.all
+    	@movement=Movement.new
+  		#@movementls=MovementCatalog.select('movement_catalogs.id','movement_catalogs.name','movements.quantity').joins('INNER JOIN movements ON movement_catalogs.id=movements.movement_catalogs_id')
+  		@movementls=MovementCatalog.select('movement_catalogs.name','movements.quantity').joins('INNER JOIN movements ON movement_catalogs.id=movements.movement_catalogs_id')
+  	end
 
-
-  def create
-		@movement=Movement.new(movement_params)
-		 if @movement.save
-		 	redirect_to root_path
-		 else
-		 	render :new
-		 end
-	end
-
-
-private
-	def movement_params
-	params.require(:movement).permit(:movement_catalogs_id, :quantity)
-	end
 end
 
 
