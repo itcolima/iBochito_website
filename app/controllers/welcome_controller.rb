4c6d1ca0bab95892feab8eef292a989ce5625912
GaboISC <11460258@itcolima.edu/mx>
2015-10-15 11:22:06 -0500

    Comentarios

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
   	def index
   		@movementcatalogs=MovementCatalog.all
-    	#@movements=Movement.all
     	@movement=Movement.new
   		#@movementls=MovementCatalog.select('movement_catalogs.id','movement_catalogs.name','movements.quantity').joins('INNER JOIN movements ON movement_catalogs.id=movements.movement_catalogs_id')
   		@movementls=MovementCatalog.select('movement_catalogs.name','movements.quantity').joins('INNER JOIN movements ON movement_catalogs.id=movements.movement_catalogs_id')
