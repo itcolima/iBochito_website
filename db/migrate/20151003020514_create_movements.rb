8ce58d91ebaa50d9ba645ddfe1a9c1788cdac638
GaboISC <11460258@itcolima.edu/mx>
2015-10-16 02:13:51 -0500

    Foreing 5

--- db/migrate/20151003020514_create_movements.rb
+++ db/migrate/20151003020514_create_movements.rb
@@ -1,7 +1,7 @@
 class CreateMovements < ActiveRecord::Migration
   def change
     create_table :movements do |t|
-      t.references :movement_catalogs, index: true, foreign_key: true
+      t.references :movement_catalogs, index: true #, foreign_key: true
       t.integer :quantity
       t.timestamps null: false
     end
